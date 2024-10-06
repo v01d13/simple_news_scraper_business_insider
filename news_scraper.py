04f1cec86ce6b15342fa98a82be87a09b6e23088
mail.prabin.shrestha@gmail.com <mail.prabin.shrestha@gmail.com>
2024-10-06 08:45:14 -0400

    details scraper for business insider

--- news_scraper.py
+++ news_scraper.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup as Soup
 import requests
 import pandas as pd
 from typing import List
+from pathlib import Path
+import pandas as pd
+import os
+import json
 
 
 def financialNewsScraper(companies: List[str], pages: int = 10):
@@ -18,7 +22,7 @@ def financialNewsScraper(companies: List[str], pages: int = 10):
         - The function uses the `pandas` library to handle data and save it to CSV files.
     """
 
-    df = pd.DataFrame(columns=["date_time", "title", "source", "link"])
+    df = pd.DataFrame(columns=["company", "date_time", "title", "source", "link"])
 
     counter = 0
     for company in companies:
@@ -38,10 +42,13 @@ def financialNewsScraper(companies: List[str], pages: int = 10):
                 source = article.find("span", class_="latest-news__source").text
                 link = article.find("a", class_="news-link").get("href")
 
+                if link.startswith('/'):
+                    link = f'https://markets.businessinsider.com{link}'
+
                 df = pd.concat(
                     [
                         pd.DataFrame(
-                            [[date_time, title, source, link]], columns=df.columns
+                            [[company, date_time, title, source, link]], columns=df.columns
                         ),
                         df,
                     ],
@@ -56,8 +63,57 @@ def financialNewsScraper(companies: List[str], pages: int = 10):
     print("Scraping completed.")
 
 
+def export_details(item):
+    try:
+        link = item['link']
+        output_file_name = link.split('/')[-1]  # get last part of the url
+        output_file_name = output_file_name.split('?')[0]  # removes url search params
+
+        output_dir = Path(f"data/{item['company']}")
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        output_path = f"{str(output_dir)}/{output_file_name}.json"
+
+        if os.path.exists(output_path):
+            return
+        
+        details = requests.get(link)
+        soup = Soup(details.text, 'lxml')
+        
+        if link.startswith('https://markets.businessinsider.com/'):
+            details = soup.find('div', class_='news-content')
+        elif link.startswith('https://seekingalpha.com/'):
+            return
+        else:
+            return
+        
+        if details is None:
+            return
+    
+        with open(output_path, 'w') as f:
+            json.dump({
+                'company': item['company'],
+                'date_time': item['date_time'],
+                'title': item['title'],
+                'link': link,
+                'details': details.get_text()
+            }, f)
+            print(output_path)
+    except Exception as ex:
+        print(item['link'], ex)
+
+
+def details_scraper():
+    for entry in os.scandir('data'):
+        if entry.name.endswith('.csv'):
+            df = pd.read_csv(entry.path)
+            for _, item in df.iterrows():
+                export_details(item)
+    
+
 def main():
-    financialNewsScraper(["amzn", "aapl", "msft", "nvda"])
+    # financialNewsScraper(["amzn", "aapl", "msft", "nvda"], 1)
+    details_scraper()
 
 
 if __name__ == "__main__":
