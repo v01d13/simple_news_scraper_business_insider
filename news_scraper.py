7fff7538eb65148a2bc8f0af6417d1f2444ba3e3
mail.prabin.shrestha@gmail.com <mail.prabin.shrestha@gmail.com>
2024-10-10 08:28:50 -0400

    handle common details page, add csv export

--- news_scraper.py
+++ news_scraper.py
@@ -10,6 +10,9 @@ import re
 from collections import Counter
 from multiprocessing import Pool
 
+
+companies = ["amzn", "aapl", "msft", "nvda", 'ibm', 'nke','mmm','ba','crm','tsla']
+
 def financialNewsScraper(companies: List[str], pages: int = 10):
     """
     Scrapes financial news articles for a list of companies from Business Insider.
@@ -68,7 +71,7 @@ def financialNewsScraper(companies: List[str], pages: int = 10):
             
             df.to_csv(output_filename)
 
-        print(f"{counter} number of articles scraped")
+        print(f"{company} {counter} number of articles scraped")
 
     print("Scraping completed.")
 
@@ -76,15 +79,13 @@ def financialNewsScraper(companies: List[str], pages: int = 10):
 def export_details(item):
     try:
         link = item['link']
-        output_file_name = link.split('/')[-1]  # get last part of the url
-        output_file_name = output_file_name.split('?')[0]  # removes url search params
-
-        output_dir = Path(f"data/{item['company']}")
-        output_dir.mkdir(parents=True, exist_ok=True)
-
-        output_path = f"{str(output_dir)}/{output_file_name}.json"
+        output_path = item['output_path']
 
         if os.path.exists(output_path):
+            return
+        
+        # dead links
+        if link.__contains__('feeds.reuters.com'):
             return
         
         details = requests.get(link)
@@ -102,7 +103,6 @@ def export_details(item):
     
         with open(output_path, 'w') as f:
             json.dump({
-                'company': item['company'],
                 'date_time': item['date_time'],
                 'title': item['title'],
                 'link': link,
@@ -115,31 +115,78 @@ def export_details(item):
 
 def details_scraper():
     data_list = []
+    total = 0
+    url_list = []
+
     for scrip_entry in os.scandir('data'):
         if not scrip_entry.is_dir():
             continue
 
         for entry in os.scandir(scrip_entry.path):
-            if len(data_list) > 1000:
+            if len(data_list) > 5000:
                 break
             if entry.name.endswith('.csv'):
                 df = pd.read_csv(entry.path)
                 for _, item in df.iterrows():
-                    data_list.append(item)
+
+                    link = item['link']
+                    if link in url_list:
+                        continue
+
+                    url_list.append(link)
+
+                    output_file_name = link.split('/')[-1]  # get last part of the url
+                    output_file_name = output_file_name.split('?')[0]  # removes url search params
+
+                    output_path = f"./data/details/{output_file_name}.json"
+
+                    total += 1
+                    if not os.path.exists(output_path):
+                        item['output_path'] = output_path
+                        data_list.append(item)
+                        print(len(data_list), end='\r')
+    print()
+    
 
     source_list = []
-    print(len(data_list))
+    print(total-len(data_list), total, f'Completed: {(total-len(data_list))/total:.0%}')
     for item in data_list:
         source = re.findall(r'(https?:.*\.com)', item['link'])
         source_list.extend(source)
     print(Counter(source_list))
 
-    pool = Pool(5)
+    pool = Pool(20)
     pool.map(export_details, data_list)
 
+
+def export_csv():
+
+    def add_details(link):
+        output_file_name = link.split('/')[-1]  # get last part of the url
+        output_file_name = output_file_name.split('?')[0]  # removes url search params
+        output_file_name = f'./data/details/{output_file_name}.json'
+        
+        if not os.path.exists(output_file_name):
+            return ''
+        
+        with open(output_file_name, 'r') as f:
+            details = json.load(f)
+            return details['details'].replace('\n', '').replace('\r', '').strip()
+
+    for company in companies:
+        df = pd.DataFrame(columns=['company', 'date_time', 'title', 'source', 'link', 'details'])
+        for entry in os.scandir(f'data/{company}_fin_news'):
+            entry_df = pd.read_csv(entry.path)
+            entry_df['details'] = entry_df['link'].apply(add_details)
+            df = pd.concat([df, entry_df])
+        df.to_csv(f'./data/{company}.csv')
+    
+
 def main():
-    financialNewsScraper(["amzn", "aapl", "msft", "nvda"], 40)
-    details_scraper()
+    # financialNewsScraper(companies, 50)
+    # details_scraper()
+
+    export_csv()
 
 
 if __name__ == "__main__":
